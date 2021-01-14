7b88be302feb8e02e0be6d2f95e01239b9869457
Matej Kupciha <kupciham@hotmail.com>
2021-01-14 18:40:18 +0100

    Add S3 buckets configuration

--- accountant_infra/accountant_infra_stack.py
+++ accountant_infra/accountant_infra_stack.py
@@ -1,9 +1,25 @@
-from aws_cdk import core
+from aws_cdk import aws_s3 as s3, core as cdk
 
 
-class AccountantInfraStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+class AccountantInfraStack(cdk.Stack):
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket_uploads = s3.Bucket(
+            self,
+            "accountant-document-uploads",
+            bucket_name="accountant-document-uploads",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            versioned=False,
+        )
+        bucket_results = s3.Bucket(
+            self,
+            "accountant-document-results",
+            bucket_name="accountant-document-results",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            versioned=False,
+        )
