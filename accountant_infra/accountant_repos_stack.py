dc5d2cf30dc87d2ca430b03f0d93c7a6060463a5
Matej Kupciha <kupciham@hotmail.com>
2021-01-18 16:49:41 +0100

    Remove untagged images automatically from ECR

--- accountant_infra/accountant_repos_stack.py
+++ accountant_infra/accountant_repos_stack.py
@@ -9,9 +9,20 @@ class AccountantReposStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # ECR repositories
+        remove_untagged_images = ecr.LifecycleRule(
+            description="Remove untagged images",
+            max_image_count=1,
+            tag_status=ecr.TagStatus.UNTAGGED,
+        )
         self.repository_web = ecr.Repository(
-            self, "accountant-web-repository", repository_name="accountant-web"
+            self,
+            "accountant-web-repository",
+            repository_name="accountant-web",
+            lifecycle_rules=[remove_untagged_images],
         )
         self.repository_worker = ecr.Repository(
-            self, "accountant-worker-repository", repository_name="accountant-worker"
+            self,
+            "accountant-worker-repository",
+            repository_name="accountant-worker",
+            lifecycle_rules=[remove_untagged_images],
         )
