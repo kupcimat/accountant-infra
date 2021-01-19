046df9816c078cf303ddd5579af807c4dbd50bdb
Matej Kupciha <kupciham@hotmail.com>
2021-01-19 13:21:32 +0100

    Update ECR repository lifecycle rules

--- accountant_infra/accountant_repos_stack.py
+++ accountant_infra/accountant_repos_stack.py
@@ -9,20 +9,21 @@ class AccountantReposStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # ECR repositories
-        remove_untagged_images = ecr.LifecycleRule(
-            description="Remove untagged images",
-            max_image_count=1,
-            tag_status=ecr.TagStatus.UNTAGGED,
+        remove_old_images = ecr.LifecycleRule(
+            description="Expire old images, keep 2 latest",
+            rule_priority=1,
+            max_image_count=2,
+            tag_status=ecr.TagStatus.ANY,
         )
         self.repository_web = ecr.Repository(
             self,
             "accountant-web-repository",
             repository_name="accountant-web",
-            lifecycle_rules=[remove_untagged_images],
+            lifecycle_rules=[remove_old_images],
         )
         self.repository_worker = ecr.Repository(
             self,
             "accountant-worker-repository",
             repository_name="accountant-worker",
-            lifecycle_rules=[remove_untagged_images],
+            lifecycle_rules=[remove_old_images],
         )
